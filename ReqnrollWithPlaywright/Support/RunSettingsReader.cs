de9f742f390ca06e903187be43ffc3161d7b4c5d
Zabi <mohamad.zabiullak@siemens-healthineers.com>
2026-03-01 07:39:31 +0530

    update RunSettingReader to thread safe

--- ReqnrollWithPlaywright/Support/RunSettingsReader.cs
+++ ReqnrollWithPlaywright/Support/RunSettingsReader.cs
@@ -1,48 +1,39 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml.Linq;
 
 namespace ReqnrollWithPlaywright.Support
 {
     public static class RunSettingsReader
     {
+        private static readonly Lazy<Dictionary<string, string>> _settings = new(LoadSettings);
 
-        private static Dictionary<string, string> _settings;
-        private static bool _isLoaded = false;
-
-        private static void Load()
+        private static Dictionary<string, string> LoadSettings()
         {
-            _settings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var settings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
             string filePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Settings.runsettings"));
             try
             {
                 var doc = XDocument.Load(filePath);
                 foreach (var param in doc.Descendants("Parameter"))
                 {
-                    var name = param.Attribute("name")?.Value;
+                    var name  = param.Attribute("name")?.Value;
                     var value = param.Attribute("value")?.Value;
 
                     if (!string.IsNullOrEmpty(name))
-                        _settings[name] = value;
+                        settings[name] = value;
                 }
-
-                _isLoaded = true;
             }
             catch (Exception ex)
             {
                 Assert.Fail($"Error loading runsettings file: {ex.Message} \n {ex.StackTrace}");
             }
+            return settings;
         }
 
-        // Get the value by key
         public static string Get(string key)
         {
-            if (!_isLoaded)
-                Load();
-            Assert.That(_settings.ContainsKey(key), Is.True , "Invalid Key! please check the .runsetting file");
-            return _settings.TryGetValue(key, out var value) ? value : null;
+            Assert.That(_settings.Value.ContainsKey(key), Is.True, "Invalid Key! please check the .runsetting file");
+            return _settings.Value.TryGetValue(key, out var value) ? value : null;
         }
     }
 }
